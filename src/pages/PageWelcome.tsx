f603759113f3b8da5226b3eb692fa799c0576933
Edward Tanguay (windows) <edward@tanguay.info>
2024-12-23 18:29:12 +0100

    start 5

--- src/pages/PageWelcome.tsx
+++ src/pages/PageWelcome.tsx
@@ -1,9 +1,40 @@
-import { useTypedStoreState } from "../store/hooks"
+import { useEffect, useState } from "react";
+import { Book } from "../types";
 
 export const PageWelcome = () => {
-	const { message } = useTypedStoreState((state) => state.mainModel);
+	const [books, setBooks] = useState<Book[]>([]);
+
+	useEffect(() => {
+		(async () => {
+			const response = await fetch("http://localhost:3344/book");
+			const _books: Book[] = await response.json();
+			setBooks(_books);
+		})();
+	}, []);
 
 	return (
-		<p>{message}</p>
-	)
-}
+		<>
+			{books.length > 0 ? (
+				<>
+					<h2 className="text-2xl mb-3">{books.length} Books</h2>
+
+					{books.map((book) => {
+						return (
+							<div key={book.id}>
+								<p>
+									{book.title} (
+									{book.completed
+										? "completed"
+										: "still reading"}
+									)
+								</p>
+							</div>
+						);
+					})}
+				</>
+			) : (
+				<p>Loading...</p>
+			)}
+		</>
+	);
+};
